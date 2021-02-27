c328504f21171d588fee2aac670cccc71a2d36e5
Lai-YT <381xvmvbib@gmail.com>
2021-02-27 19:51:04 +0800

    C++ style array start with index 0

--- chap2/insert_sort.h
+++ chap2/insert_sort.h
@@ -1,10 +1,11 @@
 #include <vector>
+
 // pseudocode at p.41
-void insertionSort(std::vector<int>& a) {
-  for (size_t j = 0; j < a.size(); ++j) {
+void insertSort(std::vector<int>& a) {
+  for (size_t j = 1; j < a.size(); ++j) {
     int key = a[j];
     int i = j - 1;
-    while (i > 0 && a[i] > key) {
+    while (i >= 0 && a[i] > key) {
       a[i + 1] = a[i];
       i = i - 1;
     }
--- chap2/main.cpp
+++ chap2/main.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 #include <vector>
 
-#include "insertion_sort.h"
+#include "insert_sort.h"
 
 // array at p.43
 int main(int argc, char const *argv[]) {
   std::vector<int> a = {31, 41, 59, 26, 41, 58};
-  insertionSort(a);
+  insertSort(a);
+
   for (int n : a) {
     std::cout << n << ' ';
   }
   std::cout << '\n';
+
   return 0;
 }
