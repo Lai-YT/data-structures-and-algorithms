1ac5665ff7a650ab5b01eaf256e9a6955546f848
Lai-YT <381xvmvbib@gmail.com>
2021-04-18 00:04:50 +0800

    implementation of circular queue

---
+++ queue/queue.hpp
@@ -0,0 +1,44 @@
+#include <iostream>
+// this is a circular-queue
+template<typename T>
+class Queue {
+public:
+  Queue(const int capacity)
+  :capacity_(capacity + 1),front_(0),rear_(0) {
+    q_ = new T[capacity_];
+  }
+
+  ~Queue() {
+    delete[] q_;
+  }
+
+  void Enqueue(const T& data) {
+    // is full
+    if (front_ - rear_ == 1) return;
+    q_[rear_] = data;
+    rear_ = (rear_ + 1) % capacity_;
+  }
+
+  void Dequeue() {
+    if (IsEmpty()) return;
+    front_ = (front_ + 1) % capacity_;
+  }
+
+  T front() const {
+      return q_[front_];
+  }
+
+  int size() const {
+    return (capacity_ + (rear_ - front_)) % capacity_;
+  }
+
+  bool IsEmpty() const {
+    return front_ == rear_;
+  }
+
+private:
+  T* q_;
+  int front_;
+  int rear_;
+  int capacity_;
+};
---
+++ queue/unittest.cpp
@@ -0,0 +1,31 @@
+#include "queue.hpp"
+
+#include <gtest/gtest.h>
+
+TEST(QueueTest, Enqueue) {
+  Queue<int> q(5);
+  q.Enqueue(1);
+  q.Enqueue(2);
+  q.Enqueue(3);
+  ASSERT_EQ(1, q.front());
+  ASSERT_EQ(3, q.size());
+}
+
+TEST(QueueTest, EnqueueAndDequeue) {
+  Queue<int> q(1);
+  ASSERT_EQ(0, q.size());
+  for (size_t i = 0; i < 1000; i++) {
+    q.Enqueue(i);
+    ASSERT_EQ(i, q.front());
+    ASSERT_EQ(1, q.size());
+    ASSERT_FALSE(q.IsEmpty());
+    q.Dequeue();
+    ASSERT_EQ(0, q.size());
+    ASSERT_TRUE(q.IsEmpty());
+  }
+}
+
+int main(int argc, char *argv[]) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
