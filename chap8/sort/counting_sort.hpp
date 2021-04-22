386ae0ff26a1b7b7c4b6f51155fa1b23f38dd28d
Lai-YT <381xvmvbib@gmail.com>
2021-04-22 20:07:00 +0800

    check stability

--- chap8/sort/counting_sort.hpp
+++ chap8/sort/counting_sort.hpp
@@ -2,6 +2,7 @@
 #define COUNTING_SORT_HPP_
 
 #include <algorithm>
+#include <utility>
 #include <vector>
 
 // at p.195
@@ -18,6 +19,26 @@ void CountingSort(const std::vector<int>& a, std::vector<int>& b, const int max)
     // minus 1 because start from 0, text book start from 1
     b.at(c.at(*itr) - 1) = *itr;
     --c.at(*itr);
+  }
+}
+
+template<typename T>
+void CountingSort(
+    const std::vector<std::pair<int, T>>& a,
+    std::vector<std::pair<int, T>>& b,
+    const int max)
+{
+  std::vector<int> c(max + 1, 0);
+  for (auto n : a) {
+    ++(c.at(n.first));
+  }
+  for (int i = 1; i <= max; ++i) {
+    c.at(i) += c.at(i - 1);
+  }
+  for (auto itr = a.rbegin(); itr != a.rend(); ++itr) {
+    // minus 1 because start from 0, text book start from 1
+    b.at(c.at(itr->first) - 1) = *itr;
+    --c.at(itr->first);
   }
 }
 
--- chap8/unittest/unittest.cpp
+++ chap8/unittest/unittest.cpp
@@ -2,6 +2,7 @@
 
 #include <algorithm>
 #include <gtest/gtest.h>
+#include <utility>
 #include <vector>
 
 #include "../../chap2/util/random.hpp"
@@ -35,6 +36,30 @@ TEST(CountingTest, Random) {
   for (size_t i = 0; i < a.size(); i++) {
     ASSERT_EQ(a.at(i), b.at(i));
   }
+}
+
+TEST(CountingTest, Stability) {
+  std::vector<std::pair<int, char>> a{
+    {2, 'a'}, {5, 'a'}, {3, 'a'}, {0, 'a'}, {2, 'b'}, {3, 'b'}, {0, 'b'}, {3, 'c'}
+  }, b(8);
+  CountingSort(a, b, 5);
+  ASSERT_EQ(0, b.at(0).first);
+  ASSERT_EQ(0, b.at(1).first);
+  ASSERT_EQ(2, b.at(2).first);
+  ASSERT_EQ(2, b.at(3).first);
+  ASSERT_EQ(3, b.at(4).first);
+  ASSERT_EQ(3, b.at(5).first);
+  ASSERT_EQ(3, b.at(6).first);
+  ASSERT_EQ(5, b.at(7).first);
+
+  ASSERT_EQ('a', b.at(0).second);
+  ASSERT_EQ('b', b.at(1).second);
+  ASSERT_EQ('a', b.at(2).second);
+  ASSERT_EQ('b', b.at(3).second);
+  ASSERT_EQ('a', b.at(4).second);
+  ASSERT_EQ('b', b.at(5).second);
+  ASSERT_EQ('c', b.at(6).second);
+  ASSERT_EQ('a', b.at(7).second);
 }
 
 int main(int argc, char *argv[]) {
