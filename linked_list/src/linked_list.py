90b72ffeb28aa027b9bfa91187fce6467c658147
Lai-YT <381xvmvbib@gmail.com>
2022-01-11 21:50:17 +0800

    Use decorator for getter and setters

--- linked_list/src/linked_list.py
+++ linked_list/src/linked_list.py
@@ -1,18 +1,30 @@
-from __future__ import annotations
+# mypy: --no-strict-optional
 
+from __future__ import annotations
+from typing import TypeVar
+
+
+T = TypeVar('T')
 
 class Node:
-    def __init__(self, data: type) -> None:
-        self._value: type = data
+    def __init__(self, value: T) -> None:
+        self._value: T = value
         self._next: Node = None
 
-    def value(self) -> type:
+    @property
+    def value(self) -> T:
         return self._value
 
+    @value.setter
+    def value(self, new_value: T) -> None:
+        self._value = new_value
+
+    @property
     def next(self) -> Node:
         return self._next
 
-    def set_next(self, next_node: Node) -> None:
+    @next.setter
+    def next(self, next_node: Node) -> None:
         self._next = next_node
 
 
@@ -21,24 +33,22 @@ class LinkedList:
         self._head: Node = None
         self._tail: Node = None
 
-    def insert(self, data: type) -> None:
+    def insert(self, data: T) -> None:
         new_node = Node(data)
         if not self._head:
             self._head = new_node
             self._tail = new_node
             return
-        self._tail.set_next(new_node)
-        self._tail = self._tail.next()
+        self._tail.next = new_node
+        self._tail = self._tail.next
 
+    @property
     def head(self) -> Node:
         return self._head
 
+    @property
     def tail(self) -> Node:
         return self._tail
 
     def is_empty(self) -> bool:
-        return self._head == None
-
-
-if __name__ == '__main__':
-    main()
+        return self._head is None
--- linked_list/test/test_linked_list.py
+++ linked_list/test/test_linked_list.py
@@ -4,45 +4,45 @@ import src.linked_list as ll
 
 
 class NodeTestCase(unittest.TestCase):
-    def setUp(self):
-        self.node: Node = ll.Node(25)
+    def setUp(self) -> None:
+        self.node = ll.Node(25)
 
-    def test_value(self):
-        self.assertEqual(25, self.node.value())
+    def test_value(self) -> None:
+        self.assertEqual(25, self.node.value)
 
-    def test_next(self):
-        self.node.set_next(ll.Node(20))
-        next_node: Node = self.node.next()
+    def test_next(self) -> None:
+        self.node.next = ll.Node(20)
+        next_node = self.node.next
 
-        self.assertEqual(20, next_node.value())
+        self.assertEqual(20, next_node.value)
 
 
 class LinkedListTestCase(unittest.TestCase):
-    def setUp(self):
+    def setUp(self) -> None:
         self.linked_list = ll.LinkedList()
 
-    def test_init(self):
-        self.assertIsNone(self.linked_list.head())
-        self.assertIsNone(self.linked_list.tail())
+    def test_init(self) -> None:
+        self.assertIsNone(self.linked_list.head)
+        self.assertIsNone(self.linked_list.tail)
 
-    def test_insert(self):
+    def test_insert(self) -> None:
         # insert one
         self.linked_list.insert(25)
-        self.assertIsNotNone(self.linked_list.head())
-        self.assertIsNotNone(self.linked_list.tail())
-        self.assertEqual(25, self.linked_list.head().value())
-        self.assertEqual(25, self.linked_list.tail().value())
+        self.assertIsNotNone(self.linked_list.head)
+        self.assertIsNotNone(self.linked_list.tail)
+        self.assertEqual(25, self.linked_list.head.value)
+        self.assertEqual(25, self.linked_list.tail.value)
         # insert two
         self.linked_list.insert(11)
-        self.assertEqual(25, self.linked_list.head().value())
-        self.assertEqual(11, self.linked_list.tail().value())
+        self.assertEqual(25, self.linked_list.head.value)
+        self.assertEqual(11, self.linked_list.tail.value)
 
-    def test_mult_insert(self):
+    def test_mult_insert(self) -> None:
         for i in range(100):
             self.linked_list.insert(i)
-            self.assertEqual(i, self.linked_list.tail().value())
+            self.assertEqual(i, self.linked_list.tail.value)
 
-    def test_is_empty(self):
+    def test_is_empty(self) -> None:
         self.assertTrue(self.linked_list.is_empty())
         self.linked_list.insert(0)
         self.assertFalse(self.linked_list.is_empty())
