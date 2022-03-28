6567f1c85e49f1e5eab0b289bc0f2b221097e29f
Lai-YT <381xvmvbib@gmail.com>
2022-03-28 23:39:47 +0800

    Fix missing header guard & Add encapsulation

--- linked_list/cpp/src/node.hpp
+++ linked_list/cpp/src/node.hpp
@@ -1,9 +1,27 @@
+#ifndef SRC_NODE_HPP_
+#define SRC_NODE_HPP_
 
 
-struct Node {
+class Node {
+public:
   Node(int value)
       : value(value) {}
 
+  /* prevent accidental modification */
+  const Node* next() const {
+    return next_;
+  }
+
+  void SetNext(Node* node) {
+    next_ = node;
+  }
+
+  /* value is intentionally let public for update */
   int value;
-  Node* next = nullptr;
+
+private:
+  Node* next_ = nullptr;
 };
+
+
+#endif /* end of include guard: SRC_NODE_HPP_ */
--- linked_list/cpp/test/ut_node.hpp
+++ linked_list/cpp/test/ut_node.hpp
@@ -7,17 +7,15 @@ TEST(NodeTest, Value) {
   Node node{25};
 
   ASSERT_EQ(25, node.value);
-  ASSERT_FALSE(node.next);
+  ASSERT_FALSE(node.next());
 }
 
 
 TEST(NodeTest, Next) {
   Node node{25};
-  node.next = new Node(20);
+  Node next_node{20};
+  node.SetNext(&next_node);
 
-  ASSERT_TRUE(node.next);
-  ASSERT_EQ(20, node.next->value);
-  
-  delete node.next;
-  node.next = nullptr;
+  ASSERT_TRUE(node.next());
+  ASSERT_EQ(20, node.next()->value);
 }
